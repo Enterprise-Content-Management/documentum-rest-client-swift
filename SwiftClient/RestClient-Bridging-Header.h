a7e66b6cd2cad6336b618788263a833c9b3c29da
Michyo Song <michyosyn@gmail.com>
2016-08-12 10:18:51 +0800

    Fix conflict SWRevealView
    Make this dependency only install by Pods.
    Build file needs to be copied to right place.

--- SwiftClient/RestClient-Bridging-Header.h
+++ SwiftClient/RestClient-Bridging-Header.h
@@ -1,5 +1,7 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
+//  Copyright © 2016 EMC Corporation. All rights reserved.
+//
 
 #import "SWRevealViewController.h"
